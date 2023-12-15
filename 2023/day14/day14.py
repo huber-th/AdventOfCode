42f295ae9bea82c81f839cc134c18d44961392d4
Thomas Huber <113915837+huber-th@users.noreply.github.com>
2023-12-14 18:40:46 -0800

    AoC 2023 Day 14 (Part 2)

--- 2023/day14/day14.py
+++ 2023/day14/day14.py
@@ -12,16 +12,44 @@ def load_data(file: str):
             for i, ch in enumerate(row):
                 columns[i] += ch
 
-    return columns
+        ncol = []
+        for row in columns:
+            row = list(row)
+            ncol.append(row)
+
+    return ncol
+
+def rotate_90_degree_clckwise(matrix):
+    new_matrix = []
+    for i in range(len(matrix[0])):
+        li = list(map(lambda x: x[i], matrix))
+        li.reverse()
+        new_matrix.append(li)
+
+    return new_matrix
 
 
-if __name__ == '__main__':
-    data = load_data('input')
+def rotate_90_degree_anticlckwise(matrix):
+    new_matrix = []
+    for i in range(len(matrix[0]), 0, -1):
+        new_matrix.append(list(map(lambda x: x[i-1], matrix)))
 
-    print('Part one:')
+    return new_matrix
+
+
+def print_matrix(matrix):
+    print('------')
+    for i in range(0,len(matrix[0])):
+        row = ''
+        for j in range(0,len(matrix)):
+            row += matrix[j][i]
+        print(row)
+    print('------')
+
+
+def shift_rocks(matrix):
     shifted = []
-    for col in data:
-        col = list(col)
+    for col in matrix:
         ncol = col
         to_move = []
         for i, ch in enumerate(reversed(col), 1):
@@ -41,13 +69,35 @@ if __name__ == '__main__':
                     idx -= 1
                 to_move = []
         shifted.append(ncol)
+    return shifted
 
+
+def rotate_cycle(matrix):
+    for _ in range(0,4):
+        matrix = shift_rocks(matrix)
+        matrix = rotate_90_degree_anticlckwise(matrix)
+    return matrix
+
+
+def calc_load(matrix):
     load = 0
-    for col in shifted:
-        for i,ch in enumerate(reversed(col), 1):
-            if ch == 'O':
-                load += i
+    for col in matrix:
+         for i,ch in enumerate(reversed(col), 1):
+             if ch == 'O':
+                 load += i
+    return load
 
-    print(load)
+
+if __name__ == '__main__':
+    data = load_data('input')
+    print('Part one:')
+    print(calc_load(shift_rocks(data)))
 
     print('Part two:')
+    data = load_data('input')
+    # Assumption that there is a loop at some point.
+    # No need to run for 1,000,000,000 cycles
+    n = 1000
+    for i in range(0,n):
+        data = rotate_cycle(data)
+    print(calc_load(data))
