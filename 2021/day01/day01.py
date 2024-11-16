082306cdc996fc94c286a3b438392584ab2e93da
Thomas Huber <113915837+huber-th@users.noreply.github.com>
2024-11-16 00:25:20 -0800

    AoC 2021 Day 01

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -6,18 +6,20 @@ def load_data(file: str):
     """ Load and sanitize data """
     p = Path(__file__).with_name(file)
     with p.open('r', encoding='utf8') as f:
-        c = f.read().strip()
+        c = [int(x) for x in f.read().strip().split('\n')]
     return c
 
 
 def main():
     ''' Entry point '''
-    data = load_data('test')
-    print(data)
+    data = load_data('input')
 
     print('Part one:')
+    print(sum(1 for prev, curr in zip(data, data[1:]) if curr > prev))
 
     print('Part two:')
+    print(sum(1 for a, b, c, d in
+              zip(data, data[1:], data[2:], data[3:]) if a+b+c < b+c+d))
 
 
 if __name__ == '__main__':
